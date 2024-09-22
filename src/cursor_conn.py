6b2324a5835600535cd9dd554659e35f408272f2
EbaraKoji <kojieva5270@gmail.com>
2024-09-22 16:17:11 +0900

    use context manager

--- src/cursor_conn.py
+++ src/cursor_conn.py
@@ -1,50 +1,43 @@
 import psycopg
 
-conn = psycopg.connect(
+with psycopg.connect(
     host='localhost',
     port=5432,
     dbname='postgres',
     user='postgres',
     password='pg_secret',
-    cursor_factory=psycopg.ClientCursor  # https://stackoverflow.com/questions/78410960/psycopg3-mogrify-attributeerror-connection-object-has-no-attribute-mogrify
-)
+    cursor_factory=psycopg.ClientCursor,  # https://stackoverflow.com/questions/78410960/psycopg3-mogrify-attributeerror-connection-object-has-no-attribute-mogrify
+) as conn:
+    with conn.cursor() as cur:
+        cur.execute('DROP TABLE IF EXISTS book;')
 
-cur = conn.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS book (
+            id INT PRIMARY KEY,
+            title VARCHAR(255),
+            pages INT,
+            author VARCHAR(255)
+        );
+        """)
 
-cur.execute('DROP TABLE book;')
+        cur.execute("""INSERT INTO book (id, title, pages, author) VALUES
+        (1, 'Python in 7 days', 328, 'Ebara Koji'),
+        (2, 'Machine Learning in 1 month', 691, 'Tensor John'),
+        (3, 'FastAPI in 7 days', 125, 'Ebara Koji'),
+        (4, 'Next.js in 7 days', 287, 'Ebara Koji'),
+        (5, 'Rust in 14 days', 350, 'Crub Red');
+        """)
 
-cur.execute("""CREATE TABLE IF NOT EXISTS book (
-    id INT PRIMARY KEY,
-    title VARCHAR(255),
-    pages INT,
-    author VARCHAR(255)
-);
-""")
+        cur.execute('SELECT * FROM book;')
+        print(cur.fetchall())
 
-cur.execute("""INSERT INTO book (id, title, pages, author) VALUES
-(1, 'Python in 7 days', 328, 'Ebara Koji'),
-(2, 'Machine Learning in 1 month', 691, 'Tensor John'),
-(3, 'FastAPI in 7 days', 125, 'Ebara Koji'),
-(4, 'Next.js in 7 days', 287, 'Ebara Koji'),
-(5, 'Rust in 14 days', 350, 'Crub Red');
-""")
+        cur.execute("""SELECT * FROM book WHERE author = 'Ebara Koji';""")
+        print(cur.fetchall())
 
-cur.execute('SELECT * FROM book;')
-print(cur.fetchall())
+        sql = cur.mogrify(
+            """SELECT * FROM book WHERE starts_with(title, %s) AND pages > %s;""",
+            ('Next.js', 200),
+        )
+        print(sql)
 
-cur.execute("""SELECT * FROM book WHERE author = 'Ebara Koji';""")
-print(cur.fetchall())
-
-sql = cur.mogrify(
-    """SELECT * FROM book WHERE starts_with(title, %s) AND pages > %s;""",
-    ('Next.js', 200),
-)
-print(sql)
-
-cur.execute(sql)
-print(cur.fetchall())
-
-conn.commit()
-
-cur.close()
-conn.close()
+        cur.execute(sql)
+        print(cur.fetchall())
