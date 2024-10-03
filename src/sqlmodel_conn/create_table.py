d71e09ae84bea39dfd7ebf14f076e9c74e25a1c8
EbaraKoji <kojieva5270@gmail.com>
2024-10-03 20:34:45 +0900

    add index to Hero.secret_name

--- src/sqlmodel_conn/create_table.py
+++ src/sqlmodel_conn/create_table.py
@@ -7,7 +7,7 @@ from sqlmodel import Field, SQLModel
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    secret_name: str
+    secret_name: str = Field(index=True)
     age: int | None = None
 
 
