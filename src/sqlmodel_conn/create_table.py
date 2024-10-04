547237df3134f929e138f55f39dd219d791863ad
EbaraKoji <kojieva5270@gmail.com>
2024-10-04 13:35:33 +0900

    use relationship

--- src/sqlmodel_conn/create_table.py
+++ src/sqlmodel_conn/create_table.py
@@ -2,7 +2,7 @@ import asyncio
 from pprint import pprint
 
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import Field, SQLModel, select
+from sqlmodel import Field, Relationship, SQLModel, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 
@@ -10,6 +10,8 @@ class Team(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     headquarters: str
+
+    heroes: list['Hero'] = Relationship(back_populates='team')
 
 
 class Hero(SQLModel, table=True):
@@ -19,6 +21,7 @@ class Hero(SQLModel, table=True):
     age: int | None = Field(default=None, index=True)
 
     team_id: int | None = Field(default=None, foreign_key='team.id')
+    team: Team | None = Relationship(back_populates='heroes')
 
 
 url = 'postgresql+asyncpg://postgres:pg_secret@localhost:5432/postgres'
